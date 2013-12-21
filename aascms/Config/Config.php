9f9e9c5c78ca3914f229f03994f016a8decd5b19
theinpu <theinpu@gmail.com>
2013-12-21 20:48:19 +0400

    открвается конфиг-файл, работает массив конфигов

--- aascms/Config/Config.php
+++ aascms/Config/Config.php
@@ -9,4 +9,19 @@ namespace aascms\Config;
 
 class Config {
 
+    /**
+     * @var array
+     */
+    private $config = array();
+
+    public function __construct($configFile) {
+        if(!file_exists($configFile)) {
+            throw new \InvalidArgumentException("Config file not found");
+        }
+        $this->config = json_decode(file_get_contents($configFile), true);
+    }
+
+    public function getAll() {
+        return $this->config;
+    }
 }
--- aascms/tests/ConfigTest.php
+++ aascms/tests/ConfigTest.php
@@ -7,10 +7,31 @@
 
 namespace aascms\tests\Config;
 
+use aascms\Config\Config;
+
 class ConfigTest extends \PHPUnit_Framework_TestCase {
 
-    public function testDefaultConfig() {
+    const ConfigFile = './cfg/config.json';
+    private $configs;
 
+    public function testBaseConfig() {
+        $cfg = new Config(self::ConfigFile);
+        $this->assertInstanceOf('aascms\\Config\\Config', $cfg);
+        $this->assertEquals($this->configs, $cfg->getAll());
+    }
+
+    protected function setUp() {
+        $this->configs = array(
+            'item1' => 'value',
+            'array' => array(
+                'item1', 'item2', 'item3'
+            )
+        );
+        file_put_contents(self::ConfigFile, json_encode($this->configs));
+    }
+
+    protected function tearDown() {
+        unlink(self::ConfigFile);
     }
 }
  
